643996cc1bc23eb41380ae1a740a4066975e0436
Cahit <yusuf.tas@metu.edu.tr>
2019-02-18 15:20:28 +0300

    Added new versions of tx and rx

---
+++ rx_new/rx_new.ino
@@ -0,0 +1,38 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+
+struct Message{
+  int js0;
+  int js1;
+  int js2;
+  int js3;
+  int js_button;
+};
+Message k;
+//Message recv_data;
+int recv_data;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    radio.read(&k, sizeof(k));
+    Serial.println(k.js_button);
+  }
+}
